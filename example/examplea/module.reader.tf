e6ba448117d06e49cf06b9b814582a391e36c020
James Woolfenden <james.woolfenden@gmail.com>
2026-05-07 00:32:41 +0100

    fix: create artifact registry repository before granting reader access

--- example/examplea/module.reader.tf
+++ example/examplea/module.reader.tf
@@ -1,8 +1,16 @@
+resource "google_artifact_registry_repository" "pike" {
+  #checkov:skip=CKV_GCP_84: example does not require CSEK encryption
+  location      = "europe-west2"
+  repository_id = "pike"
+  format        = "DOCKER"
+}
+
 module "reader" {
   source = "../../"
   region = "europe-west2"
-  repo   = "pike"
+  repo   = google_artifact_registry_repository.pike.repository_id
   members = [
     "user:james.woolfenden@gmail.com",
   ]
+  depends_on = [google_artifact_registry_repository.pike]
 }
